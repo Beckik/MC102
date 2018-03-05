557dcdde5599c72bd342c1fd030c719722949c93
beckik <vicenzo.cesconetto@hotmail.com>
2018-03-05 20:31:50 -0300

    the goddamn perfect lab

---
+++ Lab18/cmake-build-debug/CMakeFiles/Lab18.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab18.dir/lab18.c.o"
+  "Lab18.pdb"
+  "Lab18"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab18.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
